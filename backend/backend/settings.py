b1c793b0ab80360a8c086431881b5c0826a5654d
Capralg <capralg@gmail.com>
2022-06-07 20:17:31 +0300

    2022_06_07_Commit_with_deploy_04

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -112,7 +112,7 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
