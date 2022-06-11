12f7d37047b9b714de91d27085e579d20e4604a2
Capralg <capralg@gmail.com>
2022-06-11 11:58:17 +0300

    2022_06_11_Commit_with_deploy_01

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -73,7 +73,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
-        'HOST': os.getenv('DB_HOST', default='db'),
+        'HOST': os.getenv('DB_HOST', default='postgres'),
         'PORT': os.getenv('DB_PORT', default=5432)
     }
 }
