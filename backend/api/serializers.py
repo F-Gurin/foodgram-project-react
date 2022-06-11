5c83b19469351c217be71b077e3abb3dca216508
Capralg <capralg@gmail.com>
2022-06-11 21:56:52 +0300

    2022_06_11_Commit_with_deploy_04

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -27,10 +27,12 @@ class UserSerializer(ModelSerializer):
             'email',
             'id',
             'username',
+            "password",
             'first_name',
             'last_name',
             'is_subscribed',
         )
+        extra_kwargs = {'password': {'write_only': True}}
         read_only_fields = 'is_subscribed',
 
     def get_is_subscribed(self, obj):
@@ -38,6 +40,17 @@ class UserSerializer(ModelSerializer):
         if user.is_anonymous or (user == obj):
             return False
         return user.subscribe.filter(id=obj.id).exists()
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 
 class UserSubscribeSerializer(UserSerializer):
