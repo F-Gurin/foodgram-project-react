a17522ec47de2e3db5aeb57941bddddb4317a0be
Capralg <capralg@gmail.com>
2022-06-12 19:08:56 +0300

    2022_06_11_Commit_with_deploy_05

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django_filters.rest_framework import FilterSet, filters
 from rest_framework.filters import SearchFilter
 
-from recipes.models import Recipe, Ingredient
+from recipes.models import Recipe
 
 User = get_user_model()
 
