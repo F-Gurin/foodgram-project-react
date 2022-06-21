2dbdd7222591afa5583640ab764f470dc2f94d9c
Capralg <capralg@gmail.com>
2022-06-21 16:49:54 +0300

    2022_06_21_After second stage of code review_02

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -24,7 +24,7 @@ class IsAdminOrReadOnly(BasePermission):
 class AuthorOrReadOnly(IsAuthenticatedOrReadOnly):
     def has_object_permission(self, request, view, obj):
         return (
-            request.method in SAFE_METHODS 
+            request.method in SAFE_METHODS
             or obj.author == request.user
         )
 
