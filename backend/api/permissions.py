9d32f76c89af6466c962f65a07c2b25bfb47dcd3
Capralg <capralg@gmail.com>
2022-06-21 16:47:12 +0300

    2022_06_21_After second stage of code review_02

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,7 +1,9 @@
-from rest_framework import permissions
+from rest_framework.permissions import SAFE_METHODS
+from rest_framework.permissions import BasePermission
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
-class IsStaffOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
+class IsStaffOrReadOnly(IsAuthenticatedOrReadOnly):
     def has_object_permission(self, request, view, obj):
         return (
             request.method in ('GET',)
@@ -10,7 +12,7 @@ class IsStaffOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
         )
 
 
-class IsAdminOrReadOnly(permissions.BasePermission):
+class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         return (
             request.method in ('GET',)
@@ -19,15 +21,14 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         )
 
 
-class AuthorOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
+class AuthorOrReadOnly(IsAuthenticatedOrReadOnly):
     def has_object_permission(self, request, view, obj):
         return (
-            request.method in ('GET',)
-            or (request.user == obj)
-            or request.user.is_staff
+            request.method in SAFE_METHODS 
+            or obj.author == request.user
         )
 
 
-class IsAuthor(permissions.IsAuthenticatedOrReadOnly):
+class IsAuthor(IsAuthenticatedOrReadOnly):
     def has_object_permission(self, request, view, obj):
         return (request.user == obj)
